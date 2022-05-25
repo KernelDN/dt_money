41523d38cd2f61d56ffa727e3e78dd3da7849e99
Diego Natalo <diegonatalo@gmail.com>
2022-05-25 15:34:31 -0300

    add files

---
+++ dtmoney/src/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'teste1',
+          type: 'deposit',
+          category: 'test',
+          amount: 1000,
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'teste2',
+          type: 'withdraw',
+          category: 'test2',
+          amount: 500,
+          createdAt: new Date()
+        }
+      ],
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      
+      return schema.create('transaction', data)
+    })
+  }
+})
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
